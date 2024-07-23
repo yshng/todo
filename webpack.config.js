e63b7b660786c886097fb5f5a41b734d62c4ee96
yshng <107883486+yshng@users.noreply.github.com>
2024-07-23 12:55:56 -0500

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      source: "src/index.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader","css-loader"],
+      }
+    ]
+  }
+};
