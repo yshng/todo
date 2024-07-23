b71c2cd598d6280dae810e927402a7c0a8ebfa4e
yshng <107883486+yshng@users.noreply.github.com>
2024-07-23 16:53:59 -0500

    feat: generate sample card on click "new item"

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 import './styles.css';
+import {ToDo, createItem, createCard} from './todo-item';
+
+export const master: ToDo[] = [];
+
+const newItemBtn = document.querySelector("#new-item") as HTMLElement;
+newItemBtn?.addEventListener("click", function pushItem() {
+  const newItem = createItem();
+  master.push(newItem);
+  updateDisplay();
+});
+
+export function updateDisplay() {
+  const container = document.querySelector("#content") as HTMLDivElement;
+  const content = document.createElement("div");
+  master.map(function addCard(item) {
+    const card = createCard(item);
+    content.appendChild(card);
+  });
+  container.replaceChildren(content);
+}
--- src/todo-item.ts
+++ src/todo-item.ts
@@ -4,38 +4,82 @@ export interface ToDo {
   dueDate: Date | undefined;
   priority: number; // from 1 to 5, 5 being most important
   notes: string;
-  checklist: ToDo[];
+  checklist: string[];
+  //status: "unsaved" | "saved";
 }
 
-export interface Project {
-  title: string;
-  description: string;
-  items: ToDo[];
-}
-
-const defaultProject: Project = {
-  title: "",
-  description: "",
-  items: []
-} 
-
-export const projects: Project[] = [defaultProject];
-export let currentProject = projects[0];
-
-function createItem(): ToDo {
+export function createItem(): ToDo {
   const newItem: ToDo = {
-    title: "",
-    description: "",
-    dueDate: undefined,
+    title: "Test",
+    description: "Test",
+    dueDate: new Date("2001-01-01"),
     priority: 3,
-    notes: "",
-    checklist: [] 
+    notes: "More testing even than you thought.",
+    checklist: ["Some item","Next item","Check me out"],
+    //status: "unsaved" 
   }
   return newItem;
 }
 
-const newItemBtn = document.querySelector("#new-item") as HTMLElement;
-newItemBtn?.addEventListener("click", () => {
-  const newItem = createItem();
-  currentProject.items.push(newItem);
-});
+export function createCard(item: ToDo): HTMLDivElement {
+  const card = document.createElement("div");
+  card.classList.add("todo");
+
+  const title = document.createElement("p");
+  title.classList.add("title");
+  title.textContent = item.title;
+
+  const description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = item.description;
+
+  const dueDate = document.createElement("p");
+  dueDate.classList.add("due-date");
+  dueDate.textContent = String(item.dueDate);
+
+  const priorityHead = document.createElement("p");
+  priorityHead.classList.add("priority-head");
+  priorityHead.textContent = "Priority: ";
+  const priority = createPriorityDropdown();
+
+  const notesHead = document.createElement("p");
+  notesHead.classList.add("notes-head");
+  notesHead.textContent = "Notes";
+  const notes = document.createElement("p");
+  notes.textContent = item.notes;
+  const checklist = createChecklist(item.checklist);
+
+  card.append(
+    title,
+    description,
+    dueDate,
+    priorityHead,
+    priority,
+    notesHead,
+    notes,
+    checklist
+  );
+
+  return card;
+}
+
+function createPriorityDropdown(): HTMLSelectElement {
+  const list = document.createElement("select");
+  const priorities = ["very low", "low", "moderate", "high", "very high"];
+  for (let i = 0; i <= 4; i++) {
+    const option = document.createElement("option");
+    option.textContent = priorities[i];
+    list.appendChild(option);
+  }
+  return list;
+}
+
+function createChecklist(array: string[]): HTMLUListElement {
+  const list = document.createElement("ul");
+  array.map((listitem) => {
+    const li = document.createElement("li");
+    li.textContent = listitem;
+    list.appendChild(li);
+  });
+  return list;
+}
