b30a0e99d9ee464e168a03a08e06667c685008a2
yshng <107883486+yshng@users.noreply.github.com>
2024-09-10 09:58:16 -0500

    update imports and sample task creation to reflect new class implementation

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,22 @@
 import './card.css';
 import './layout.css';
 import './dialog.css';
-import {ToDo, Status, Timescale, createCard} from './card';
+import {ToDo, Status, Timescale} from './todo';
 import './dialog';
+import './project';
+import {createCard} from './card'
 
 export let master: ToDo[] = [];
  
-const sample1: ToDo = {
-  title: "Sample Task",
-  dueDate: new Date(2025,12,1,),
-  priority: 2,
-  notes: "Here are some notes about what I need to do",
-  status: "not started",
-  timescale: "less-hour",
-  created: Date.now()
-}
+const sample1 = new ToDo(
+  "Sample Task",
+  new Date(2025,12,1),
+  2,
+  "Here are same notes",
+  "less-hour"
+);
 
-const sample2: ToDo = {
-  title: "Sample Task 2",
-  dueDate: new Date(2025,12,2,),
-  priority: 3,
-  notes: "",
-  status: "started",
-  timescale: "days",
-  created: Date.now()
-}
-
-const sample3: ToDo = {
-  title: "Sample Task 3",
-  dueDate: new Date(2025,12,5),
-  priority: 0,
-  notes: "Maybe don't even do this task.",
-  status: "paused",
-  timescale: "less-5-min",
-  created: Date.now()
-}
-
-master.push(sample1,sample2,sample3);
+master.push(sample1);
 updateDisplay();
 
 export function updateDisplay() {
