6f6e082151d32e24cd4f9a561eb9080cfda03477
yshng <107883486+yshng@users.noreply.github.com>
2024-09-23 17:42:06 -0500

    replace outdated project code
    remove project class, add function to complie projects from existing todo cards

--- src/project.ts
+++ src/project.ts
@@ -1,44 +1,11 @@
-import { ToDo } from "./todo";
+import { todos } from ".";
 
-export class Project {
-
-title: string;
-created: Date;
-items?: ToDo[];
-
-constructor(title: string, items?: ToDo[], created?: Date){
-  this.title = title;
-  this.items = items;
-  if (created != undefined) {
-    this.created = created;
-  } else {
-    this.created = new Date();
-  }
-};
-
-addItem(item: ToDo) {
-  let added = [item];
-  if (this.items) {added = this.items.concat(added)};
-  return new Project(this.title, added, this.created);
-}
-
-removeItem(created: Date) {
-  let removed: ToDo[] = [];
-  if (this.items) {removed = this.items.filter(item => item.created != created)}
-  return new Project(this.title, removed, this.created);
-}
-
-toString(): string {
-  let list: string = "";
-  if (this.items)  {
-    for (let item of this.items) {
-      list += `\n${item.toString()}`;
+export function getProjects(): string[] {
+  let projects: string[] = [];
+  for (let {project} of todos) {
+    if (!projects.includes(project)) {
+      projects.push(project);
     }
-  } else {
-    list = "no items";
   }
-  return `${this.title} contains ${list}`;
+  return projects;
 }
-  
-}
-
