c6d3bd2942ef746df4a2eb0585003abc780aac93
yshng <107883486+yshng@users.noreply.github.com>
2024-09-24 15:11:32 -0500

    add project class and related functionality
    generate ID based on counter, add a project, add project dropdown to item creation, make "new project" button work

--- src/project.ts
+++ src/project.ts
@@ -1,11 +1,52 @@
-import { todos } from ".";
+import { State, updateDisplay } from ".";
 
-export function getProjects(): string[] {
-  let projects: string[] = [];
-  for (let {project} of todos) {
-    if (!projects.includes(project)) {
-      projects.push(project);
-    }
+export class Project {
+  #counter = 0;
+  title: string;
+  ID: string;
+
+  constructor(title: string) {
+    this.title = title;
+    let id = this.#counter.toString().padStart(5,'0');
+    this.ID = `project${id}`;
+    this.#counter++;
   }
-  return projects;
 }
+
+export function addProjectDropdown(state: State) {
+  const projectDiv = document.querySelector("#project-dropdown");
+  const head = document.createElement("p");
+  head.classList.add("project-head");
+  head.textContent = "Project";
+  const dropdown = document.createElement("select");
+  dropdown.classList.add("project-dropdown");
+  for (let {title, ID} of state.projects) {
+    const opt = document.createElement("option");
+    opt.setAttribute("value",ID);
+    opt.textContent = title;
+    dropdown.appendChild(opt);
+  }
+  const addProject = document.createElement("option");
+  const span = document.createElement("span");
+  span.classList.add("new-project");
+  span.textContent = "Add new project";
+  addProject.appendChild(span);
+  dropdown.appendChild(addProject);
+  projectDiv?.replaceChildren(head,dropdown);
+}
+
+export function addProject(state: State, title: string): State {
+  let project = new Project(title);
+  let amended = state.projects.concat(project);
+  return {...structuredClone(state),projects: amended};
+}
+
+export function addProjectButtons(state: State) {
+  const buttons = document.querySelectorAll(".new-project");
+  buttons.forEach( (button) => {
+  button.addEventListener("click", () => {
+    let title = prompt("Name your new project: ","Another Project");
+    if (title) state = addProject(state, title);
+    updateDisplay(state);
+    })
+  })}
