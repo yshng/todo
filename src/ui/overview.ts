2cc1e1628c4b9c64d62e25033c5fa095ec976632
yshng <107883486+yshng@users.noreply.github.com>
2024-10-24 13:16:14 -0500

    fix: add projectless tasks to overview

--- src/ui/overview.ts
+++ src/ui/overview.ts
@@ -6,7 +6,7 @@ import { updateDisplay } from "..";
 
 function enableOverviewButton() {
   const overviewButton = document.querySelector("#overview-button");
-  overviewButton?.addEventListener("click",() => toggleOverview());
+  overviewButton?.addEventListener("click", () => toggleOverview());
 }
 
 enableOverviewButton();
@@ -20,44 +20,44 @@ function toggleOverview() {
 }
 
 export function populateOverview() {
-
-  const container = createElement({type: "div",id: "overview"});
+  const container = createElement({ type: "div", id: "overview" });
   const projects = getProjects();
 
   for (let project of projects) {
-    if (project.id > 0) {
-    const h1 = createElement({type: "h1", classes: "overview-project-title"})
+    const h1 = createElement({ type: "h1", classes: "overview-project-title" });
     const link = createElement({
       type: "a",
-      text: project.title,
+      text: project.id == -1 ? "(no project)" : project.title,
       classes: "overview-link",
-      attr: [["href","#"]]
+      attr: [["href", "#"]],
     });
-    link.addEventListener("click", () => selectProject(project.id))
+    link.addEventListener("click", () => selectProject(project.id));
     h1.appendChild(link);
     container.appendChild(h1);
 
-    const list = createElement({type: "ul", classes: "overview-list"});
-    let todos = getToDos().filter( (todo) => todo.projectID == project.id);
+    const list = createElement({ type: "ul", classes: "overview-list" });
+    let todos = getToDos().filter((todo) => todo.projectID == project.id);
     for (let todo of todos) {
-      const li = createElement({type: "li",classes: "overview-list-item"})
+      const li = createElement({ type: "li", classes: "overview-list-item" });
       const link = createElement({
         type: "a",
         text: todo.title,
         classes: "overview-link",
-        attr: [["href",`#`]]
-      })
+        attr: [["href", `#`]],
+      });
       const span = createElement({
         type: "span",
         text: todo.status,
-        classes: "status"
-      })
-      if (todo.status == "done") {link.style.textDecoration = "line-through"};
-      link.addEventListener("click",() => selectToDo(todo.created));
+        classes: "status",
+      });
+      if (todo.status == "done") {
+        link.style.textDecoration = "line-through";
+      }
+      link.addEventListener("click", () => selectToDo(todo.created));
       li.append(link, span);
       list.appendChild(li);
     }
     container.appendChild(list);
-  }}
-    return container;
+  }
+  return container;
 }
