70545256815e64413a669cccdcf8e23bb2b38e79
yshng <107883486+yshng@users.noreply.github.com>
2024-10-24 11:17:35 -0500

    add new item button back to empty projects

--- src/ui/content.ts
+++ src/ui/content.ts
@@ -2,6 +2,7 @@ import { createCard } from "../ui/card";
 import { deleteProject, getProjectByID } from "../model/project";
 import { getCompleted, getCurrentProject, getIncomplete } from "../model/storage";
 import { confirmProjectDelete } from "../ui/delete-message";
+import { makeNewItemButton } from "./new-item-button";
 
 export function populateContent() {
   const contentDiv = document.createElement("div");
@@ -29,8 +30,10 @@ export function populateContent() {
     });
   } else if (current == -1) {
     message.textContent = "You have no pending tasks.";
+    message.appendChild(contentItemButton("Add a task"));
   } else {
     message.textContent = "This project contains no tasks.";
+    message.appendChild(contentItemButton("Add a task"));
   }
 
   if (current > 0) {
@@ -61,4 +64,11 @@ export function createBuffer() {
   const buffer = document.createElement("div");
   buffer.classList.add("buffer");
   return buffer;
+}
+
+function contentItemButton(message: string) {
+  const addItem = document.createElement("button");
+  addItem.textContent = message;
+  makeNewItemButton(addItem);
+  return addItem;
 }
